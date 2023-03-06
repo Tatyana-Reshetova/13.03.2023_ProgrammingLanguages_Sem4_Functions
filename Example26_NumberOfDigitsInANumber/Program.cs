9df5ad7171b84c6e3405746358d661b866b1e662
Tatyana_Reshetova <vlasenko2205@gmail.com>
2023-03-06 16:33:57 +0300

    Количество цифр в числе

---
+++ Example26_NumberOfDigitsInANumber/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу которая принимает на вход число и выдает количество цифр в числе
+// 456 -> 3
+//78 -> 2
+//89126 -> 5
+
+int CountNumbers(int n)
+{
+    int count = 0;
+    if (n<0)
+    n *= (-1); //n = n * (-1)
+    while (n>0)
+    {
+        n/=10; //n = n/10 
+        count++;
+    }
+    return count;  
+}
+
+Console.Clear();
+Console.Write("Введите любое число: ");
+int n = int.Parse(Console.ReadLine());
+Console.Write ($"В числе {n} количество цифр - {CountNumbers(n)}.");
+
+
