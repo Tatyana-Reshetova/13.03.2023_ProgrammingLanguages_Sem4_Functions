e4042e108da13db8fbb43b05cab0e629f228e6a9
Tatyana_Reshetova <vlasenko2205@gmail.com>
2023-03-06 16:53:40 +0300

    Произведение числе от 1 до N

---
+++ Example28_MultiplicationFrom1ToN/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напиите программу которая принимает на вход число N
+// и выдает произведение числел от 1 до N
+
+int Multiplication(int n)
+{
+    int Mult = 1;
+    for (int i = 1; i <= n ; i++)
+    Mult *= i;
+    return Mult;
+}
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine($"Произведение чисел от 1 до {n} равно {Multiplication(n)}");
+
