48d2e6aa0c969cb1b7af9712ca67d9f30cf77d8c
Tatyana_Reshetova <vlasenko2205@gmail.com>
2023-03-06 16:03:20 +0300

    Сумма чисел от 1 до А

---
+++ Example24_SumFrom1ToN/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу которая принимает на вход число А
+// и выдает сумму чисел от 1 до А
+// а = 5
+// sum = 1 + 2 + 3 + 4 + 5
+
+
+
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+// int a = GetSum(n);
+Console.WriteLine($"Сумма чисел от 1 до {n} равна {GetSum(n)}");
+
+int GetSum (int n){
+    int sum = 0;
+    for (int i = 1; i <= n; i ++)
+    sum += i;// sum = sum + i
+    return sum;
+}
